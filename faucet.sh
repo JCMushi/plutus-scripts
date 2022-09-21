b007cd2393d6df123f5ca77c3eb85da02ff62155
JCMushi <111410326+JCMushi@users.noreply.github.com>
2022-09-21 15:11:08 +0800

    Update faucet.sh

--- faucet.sh
+++ faucet.sh
@@ -1,4 +1,4 @@
 ADDRESS=$(cat ./wallets/main.addr)
 KEY=$1
-curl -v -XPOST "https://faucet.alonzo-purple.dev.cardano.org/send-money/$ADDRESS?apiKey=$KEY"
-
+#curl -v -XPOST "https://faucet.alonzo-purple.dev.cardano.org/send-money/$ADDRESS?apiKey=$KEY"
+curl -X POST -s ‘https://faucet.preprod.world.dev.cardano.org/send-money/$ADDRESS?api_key=ooseiteiquo7Wie9oochooyiequi4ooc’
